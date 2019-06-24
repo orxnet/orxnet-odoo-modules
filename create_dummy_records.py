18caad236e4b8e931dee505e115bcbdb9d0c7822
kiplangatdan <kiplangatdan@gmail.com>
2019-06-24 12:33:51 +0300

    Confirm draft orders

--- create_dummy_records.py
+++ create_dummy_records.py
@@ -8,16 +8,25 @@ odoo = odoorpc.ODOO('localhost', port=8069)
 print(odoo.db.list())
 
 # Login
-odoo.login('odoo11database', 'admin', 'admin')
+odoo.login('boekenbalie', 'admin', 'admin')
+
+# # access the sale.order model
+# if 'sale.order' in odoo.env:
+#     Order = odoo.env['sale.order']
+#     order_ids = Order.search([])
+#
+#     # ask user for the number of records to generate
+#     num = input("Enter the number of records to duplicate: ")
+#     for i in range(0, int(num)+1):
+#         order = Order.browse(random.choice(order_ids))
+#         order.copy()
+#         print("%s Successfully copied." % order.name)
 
 # access the sale.order model
 if 'sale.order' in odoo.env:
     Order = odoo.env['sale.order']
-    order_ids = Order.search([])
-
-    # ask user for the number of records to generate
-    num = input("Enter the number of records to duplicate: ")
-    for i in range(0, int(num)+1):
-        order = Order.browse(random.choice(order_ids))
-        order.copy()
-        print("%s Successfully copied." % order.name)
+    order_ids = Order.search([('state', '=', 'draft')], limit=400)
+    to_confirm = Order.browse(order_ids)
+    for order in to_confirm:
+        order.action_confirm()
+        print("%s Confirmed!" % order.name)
