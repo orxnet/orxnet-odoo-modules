563bd84aaec63df0f4faa27cfd7f4b74a62aa469
Tom Blauwendraat <antiflu@gmail.com>
2019-07-26 13:26:58 +0300

    latest buildout changes

--- scripts/create_dummy_records.py
+++ scripts/create_dummy_records.py
@@ -11,22 +11,22 @@ print(odoo.db.list())
 odoo.login('boekenbalie', 'admin', 'admin')
 
 # # access the sale.order model
-# if 'sale.order' in odoo.env:
-#     Order = odoo.env['sale.order']
-#     order_ids = Order.search([])
-#
-#     # ask user for the number of records to generate
-#     num = input("Enter the number of records to duplicate: ")
-#     for i in range(0, int(num)+1):
-#         order = Order.browse(random.choice(order_ids))
-#         order.copy()
-#         print("%s Successfully copied." % order.name)
+if 'sale.order' in odoo.env:
+     Order = odoo.env['sale.order']
+     order_ids = Order.search([])
+
+     # ask user for the number of records to generate
+     num = input("Enter the number of records to duplicate: ")
+     for i in range(0, int(num)+1):
+         order = Order.browse(random.choice(order_ids))
+         order.copy()
+         print("%s Successfully copied." % order.name)
 
 # access the sale.order model
-if 'sale.order' in odoo.env:
-    Order = odoo.env['sale.order']
-    order_ids = Order.search([('state', '=', 'draft')], limit=500)
-    to_confirm = Order.browse(order_ids)
-    for order in to_confirm:
-        order.action_confirm()
-        print("%s Confirmed!" % order.name)
+#if 'sale.order' in odoo.env:
+#    Order = odoo.env['sale.order']
+#    order_ids = Order.search([('state', '=', 'draft')], limit=500)
+#    to_confirm = Order.browse(order_ids)
+#    for order in to_confirm:
+#        order.action_confirm()
+#        print("%s Confirmed!" % order.name)
