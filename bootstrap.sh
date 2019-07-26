a6baa80f0e2d15a5d3e8f7e0e18d78a63785441e
Saskia <saskia@sunflowerweb.nl>
2019-07-26 13:52:33 +0200

    changes for saskias local

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget -qO- https://raw.githubusercontent.com/buildout/buildout/master/bootstrap/bootstrap.py | env python2 - --setuptools-version=40.0.0 --buildout-version=2.12.1
+wget -qO- https://raw.githubusercontent.com/buildout/buildout/master/bootstrap/bootstrap.py | env python3.5 - --setuptools-version=40.0.0 --buildout-version=2.12.1
 
